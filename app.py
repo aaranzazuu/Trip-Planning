d6b621e1447127a672a6a2975b09c873114625d2
Aranzazu Ballesteros <aaranzazuu@gmail.com>
2020-05-27 14:10:07 -0700

    flask on app.py working

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask
 
+# Flask Setup
+app = Flask(__name__)
+
+# Def Home
+@app.route("/")
+def home():
+    return (
+        f"Welcome to the Home Page!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
