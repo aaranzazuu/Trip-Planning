6a97bd2b057fe5b99dbf32c3b46ef58484d77030
Aranzazu Ballesteros <aaranzazuu@gmail.com>
2020-05-27 18:10:47 -0700

    start and end date work

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
     )
 
 #Import MatplotLib 
@@ -24,11 +26,11 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite",connect_args={'check_same_thread': False})
 Base = automap_base()
 Base.prepare(engine,reflect=True)
 Base.classes.keys()
-    # Save references to each table
+# Save references to each table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 session = Session(engine)
@@ -49,10 +51,11 @@ def precipitation():
 
 @app.route("/api/v1.0/stations")
 def stations():
-    active_stations = session.query(Measurement.station).group_by(Measurement.station).count()
+    #active_stations = session.query(Measurement.station).group_by(Measurement.station).count()
     from operator import itemgetter 
     list_stations = session.query(Measurement.station).group_by(Measurement.station).all()
     return jsonify(list_stations)
+
 
 @app.route("/api/v1.0/tobs")
 def tobs():
@@ -60,7 +63,40 @@ def tobs():
     for row in session.query(Measurement.tobs, Measurement.date).filter(Measurement.station=="USC00519281").filter(Measurement.date>="2016-08-23").all():
         (tobs,date) = row
         list_temp.append(tobs)
+        list_tempdate.append(date)
     return jsonify(list_temp)
+
+from markupsafe import escape
+
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+
+    lowest_temp = session.query(func.min(Measurement.tobs)).filter(Measurement.date>=str(start)).all()
+    highest_temp = session.query(func.max(Measurement.tobs)).filter(Measurement.date>=str(start)).all()
+    avg_temp = session.query(func.avg(Measurement.tobs)).filter(Measurement.date>=str(start)).all()
+    return(
+        f"The lowest temperature observation data (TOBS) on {escape(start)} was {(escape(lowest_temp[0]))}<br>"
+        f"The highest temperature observation data (TOBS) on {escape(start)} was {(escape(highest_temp[0]))}<br>"
+        f"The average temperature observation data (TOBS) on {escape(start)} was {(escape(avg_temp[0]))}<br>"
+        )
+    #return(f"Date {start} not found.")
+
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start,end):
+    lowest_temp = session.query(func.min(Measurement.tobs)).filter(Measurement.date>=str(start)).filter(Measurement.date<=str(end)).all()
+    highest_temp = session.query(func.max(Measurement.tobs)).filter(Measurement.date>=str(start)).filter(Measurement.date<=str(end)).all()
+    avg_temp = session.query(func.avg(Measurement.tobs)).filter(Measurement.date>=str(start)).filter(Measurement.date<=str(end)).all()
+
+    return(
+        f"The lowest temperature observation data (TOBS) between {escape(start)} and {escape(end)} was {(escape(lowest_temp[0]))}<br>"
+        f"The highest temperature observation data (TOBS) on {escape(start)} and {escape(end)} was {(escape(highest_temp[0]))}<br>"
+        f"The average temperature observation data (TOBS) on {escape(start)} and {escape(end)} was {(escape(avg_temp[0]))}<br>"
+        )
+    #Calculate the average temperature recorded
+    #
+    #lowest_temp
+    #return print(f" {lowest_temp}")
 
 if __name__ == "__main__":
     app.run(debug=True)
